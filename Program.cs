e144a10d578761d799cb7d8d8b68c1d7e8c39e5b
Nikan <32056409+Nikan888@users.noreply.github.com>
2018-04-14 11:21:49 +0300

    Task

--- Program.cs
+++ Program.cs
@@ -10,7 +10,23 @@ namespace Task11
     {
         static void Main(string[] args)
         {
-
+            TextTransformer textTransformer = new TextTransformer();
+            List<string> message = new List<string>
+            {
+                null,
+                "One can become a writer only if he is talented.",
+                null,
+                "",
+                null,
+                "They say, the Winter will be cold.",
+                null
+            };
+            message = textTransformer.SetMessageToUpperCase(message);
+            foreach (var t in message)
+            {
+                Console.WriteLine(t);
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ TextTransformer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Task11
+{
+    internal class TextTransformer
+    {
+        public IEnumerable<Char> SetCharToUpperCase(string input)
+        {
+            foreach (char ch in input)
+            {
+                yield return Char.ToUpper(ch);
+            }
+        }
+
+        public string SetSomethingVariable(ref string str)
+        {
+            str = "/something_variable/";
+            return str;
+        }
+
+        public List<string> SetMessageToUpperCase(List<string> message)
+        {
+            string test = "";
+
+            for (int i = 0; i < message.Count; i++)
+            {
+                if (string.IsNullOrEmpty(message[i]))
+                {
+                    test = message[i];
+                    SetSomethingVariable(ref test);
+                    message[i] = test;
+                }
+                else
+                {
+                    message[i] = new string(SetCharToUpperCase(message[i]).ToArray());
+                }
+            }
+            return message;
+        }
+    }
+}
