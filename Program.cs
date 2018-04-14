22a1665f26fc328e0db030385bdce7a82b140129
Nikan <32056409+Nikan888@users.noreply.github.com>
2018-04-14 14:44:18 +0300

    Task

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Task11
@@ -10,6 +11,15 @@ namespace Task11
     {
         static void Main(string[] args)
         {
+            //string str = null + "One can become a writer only if he is talented. " 
+            //    + null + "" + null + "They say, the Winter will be cold." + null;
+            //Regex regSplitSentence = new Regex(@"\. |\! |\? ");
+            //string[] sentences = regSplitSentence.Split(str);
+            //List<string> listSentence = new List<string>();
+            //foreach (var s in sentences)
+            //{
+            //    listSentence.Add(s);
+            //}
             TextTransformer textTransformer = new TextTransformer();
             List<string> message = new List<string>
             {
